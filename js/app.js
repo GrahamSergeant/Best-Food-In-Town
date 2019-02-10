b32f7648f1daae3ad0486e0b8ea7a1dc43a4b4c8
system_user <grahamsergeant4@gmail.com>
2019-02-10 12:36:03 +0000

    centred map on users current location

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 let myViewModel = {
-
+  
 };
-
 
 
 function init(){
   ko.applyBindings(myViewModel);
-
+  
   const myLatLng = {lat: 40.758895, lng: -73.9873197};
   let map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
       center: myLatLng,
       mapTypeControl: false
   });
+  if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(function (position) {
+         myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+         map.setCenter(myLocation);
+     });
+ }
 
 }
