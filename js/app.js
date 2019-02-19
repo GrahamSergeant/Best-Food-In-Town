f855a7d977e6f557dc6221f0dd1a6e09d35134bc
system_user <grahamsergeant4@gmail.com>
2019-02-19 13:57:01 +0000

    swapped google places API data for foursquare venue/details api data in infowindows, used gitignore to mask foursquare api keys in config file, adding markers on promise from single foursquare  api call per venue

--- js/app.js
+++ js/app.js
@@ -1,170 +1,274 @@
 //next TODO
-//style infowindows
-//bind marker select to list select
-//add warning for invalid query radius
-//add UI to specify travel mode
-//test responsive scaling
+//make foursquare api keys secret
+//sort alphabetised list by rating button
+//food type filter
+//display menu
+//search foursquare queries for even more data to display
+//put tip/username/photo on one line
+//test responsive scaling - make flexbox
+//add powered by Googlemaps and Foursquare badges
+//double marker size
+//centre map bounds on selected restaurant
+//write readme file
 
+
+//Foursquare API keys
+const CLIENT_ID = config.CLIENT_ID;
+const CLIENT_SECRET = config.CLIENT_SECRET;
+//ko bound viewmodel - called in init() function
 let ViewModel = function() {
-  //
+  //coerce value of this
   let self = this;
-  let location = {lat: 40.758895, lng: -73.9873197}; //NYC for splash screen
+  //set ko bound splash logo to visible
+  this.showSplash = ko.observable (true);
+  //declare restaurant array, ko bound to restaurant list element in html
+  this.placeList = ko.observableArray ([]);
+  //data collection - pushed out to various map api calls and updated on return
   let map;
   let markers = [];
-  let userMarker;
-  let route;
-  let selectedRestaurant;
-  let restaurantMarker;
-  //
-  this.radius = ko.observable();
-  this.orderedList = ko.observableArray ([]);
-  this.showSplash = ko.observable (true);
-  this.toggleUI = ko.observable(false);
-  //perform a new place search with new radius input
-  this.radiusUpdate = function(){
-    mapUpdate();
-  };
-  //draw route to selected restaurant
-  this.restaurantSelected = function(restaurant) {
-    //prevent selection of same resturant again
-    if (restaurant != selectedRestaurant){
-      selectedRestaurant = restaurant;
-      //remove route from last selection
-      if (route){
-        route.setMap(null);
-        markerBounceToggle(restaurantMarker);
-      }
-      //return promise from directions service API for new route
-      displayRoute(restaurant.geometry.location,map,location).then(function(result){
-        route = result;
-        //find selected restaurant marker and toggle bounce
-        restaurantMarker = markers.find(function(marker){
-          if(marker.title === restaurant.name){
-            return (marker);
-          }
+  //pull initial restaurant data from prepared object in place_data.js
+  //push each restaurant entry in places object to ko bound list element and Foursquare api
+  places().forEach(function(restaurant){
+        fakeFoursquareQuery(restaurant.name).then(function(response){
+          markers.push(addPlaceMarker(response, map));
+          restaurant.rating = response.rating;
+          self.placeList.push(restaurant);
         });
-        markerBounceToggle(restaurantMarker);
+    });
+      
+  //
+  //bounce marker of selected restaurant
+  this.bounceRestaurantMarker = function(restaurant) {
+      //find selected restaurant marker and toggle bounce
+      markers.find(function(marker){
+        if(marker.title === restaurant.name){
+          markerBounceToggle(marker);
+          setTimeout(markerBounceToggle(marker),2000);
+        }
       });
-    }
   };
   //initial map set up - called once
   function mapInit(){
     map = new google.maps.Map(document.getElementById('map'), {
       zoom: 14,
-      center: location,
+      center: {lat: 52.4813098, lng: -1.9156044},
       mapTypeControl: false,
       styles: mapStyle()
     });
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        self.showSplash(false);
-        self.toggleUI(true);
-        userMarker = addUserMarker(map,location);
-        map.setCenter(location);
-        self.radius(1000);
-        mapUpdate(self.radius());
-      });
-    } else (alert(error +'Current location not found, refresh to try again'));
   }
-  //updatable map API calls - called once by mapInit, then repeatedly by UI inputs
-  function mapUpdate(){
-    placesSearch(map,location,self.radius()).then(function(results){
-      self.orderedList([]);
-      sortResults(results).forEach(function(result){
-        self.orderedList.push(result);
-    });
-    if (route){
-      route.setMap(null);
-    }
-    if (markers[0]){
-      clearMarkers(markers);
-    }
-    markers = addPlaceMarkers(self.orderedList(),map);
-    }).catch(function(error) {
-        alert(error +'No places found, try expanding query radius');
-      });
-  }
+  //remove splash screen
+  setTimeout(function(){self.showSplash(false);},1500);
+  //initialise map
   mapInit();
+  //
 };
 
-
+//google map API callback/loaded in html file
 function init(){
   ko.applyBindings(new ViewModel());
 }
-  
-function placesSearch(map,location,radius){
+
+//spoof api call with prepared json data
+function fakeFoursquareQuery(query){
+  let uri;
+  if(query === 'Digbeth Dining Club'){
+    uri = 'https://api.myjson.com/bins/yckbu';
+  }
+  if(query === 'Adams'){
+    uri = 'https://api.myjson.com/bins/6v03m';
+  }
+  if(query === 'The Highfield'){
+    uri = 'https://api.myjson.com/bins/1e8u6i';
+  }
+  if(query === 'Fiesta del Asado'){
+    uri = 'https://api.myjson.com/bins/o3wq2';
+  }
+  if(query === 'Damascena'){
+    uri = 'https://api.myjson.com/bins/1dajm2';
+  }
   return new Promise(function(resolve,reject){
-    let request = {
-      location: location,
-      radius: radius,
-      type: ['restaurant']
-    };
-    placesServiceCall = new google.maps.places.PlacesService(map);
-    placesServiceCall.nearbySearch(request,placesSearchResults);
-    function placesSearchResults(results,status){
-      if(status==='OK'){
-        resolve(results);
-      } else alert('Search Result: '+status+' try increasing query radius');
-    }
+      fetch(uri).then(function(result) {
+        (result.json()).then(function(jsonResult){
+          resolve(jsonResult);
+        }).catch(function(error){
+          reject(alert('Failed to fetch and parse JSON data in fake API call' + error));
+        });
+      });
+    });
+}
+
+//genuine api call to foursquare - query argument is restaurant name
+function foursquareQuery(query){
+  return new Promise(function(resolve,reject){
+    //initial, shallow foursquare place search to get venue id from name and birmingham lat/lng area query
+    fetch(`https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323&ll=52.4813098,-1.9156044&query=${query}`)
+      .then(function(result) {
+        (result.json()).then(function(jsonResult){
+        //deeper foursquare venue/details search with venue id
+        let VENUE_ID = jsonResult.response.groups[0].items[0].venue.id;
+        fetch(`https://api.foursquare.com/v2/venues/${VENUE_ID}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20180323`)
+        .then(function(result){
+          (result.json()).then(function(jsonResult){
+              resolve(jsonResult.response.venue);
+          });
+        }).catch(function(error) {
+            alert('FourSquare venue/details serarch failed with error: ' + error);
+          });
+        });
+    })
+    .catch(function(error) {
+      alert('FourSquare venue search failed with error: ' + error);
+    });
   });
 }
 
-function sortResults(results){
+function addPlaceMarker(venue, map){
+  let lat = venue.location.lat;
+  let lng = venue.location.lng;
+  let location = {lat,lng};
+  let marker = new google.maps.Marker({
+      map: map,
+      icon: 'img/bfit_map_pin.png',
+      title: venue.name,
+      position: location,
+    });
+  let placeInfoWindow = new google.maps.InfoWindow();
+  marker.addListener('click', function() {
+    if (placeInfoWindow.marker != this) {
+          markerBounceToggle(this);
+          setTimeout(markerBounceToggle(marker),2000);
+          openInfoWindow(this, placeInfoWindow, venue, map);
+    }
+  });
+  return (marker);
+}
+
+function openInfoWindow(marker, infowindow, venue, map) {
+    infowindow.marker = marker;
+    let innerHTML = '<div id=' + 'infowindow_container' + '>' + '<div id=' + 'infowindow' + '>';
+    if (venue.name){
+      innerHTML += '<strong>' + venue.name + '</strong>';
+    }
+    if (venue.location.formattedAddress){
+      innerHTML += '<br>' + venue.location.formattedAddress;
+    }
+    if (venue.contact.formattedPhone){
+    innerHTML += '<br>' + venue.contact.formattedPhone;
+    }
+    if (venue.rating){
+      innerHTML += '<br>' + '<strong>Average Rating: </strong>' + venue.rating;
+    }
+    if (venue.tips.groups[0].items[0].user && venue.tips.groups[0].items[0].text) {
+      innerHTML += '<br>' + '<strong>' + venue.tips.groups[0].items[0].user.firstName + ' ' + venue.tips.groups[0].items[0].user.lastName + '\'s tip: ' + '</strong>' + ' ' + venue.tips.groups[0].items[0].text;
+    }
+    if (venue.tips.groups[0].items[0].photo) {
+      let photoPrefix = venue.tips.groups[0].items[0].photo.prefix
+      let photoSuffix = venue.tips.groups[0].items[0].photo.suffix
+      let photoURL = photoPrefix + '100x100' + photoSuffix;
+        innerHTML += '<br><br><img src="' + photoURL + '">';
+      }
+    innerHTML += '</div>' + '</div>';
+    infowindow.setContent(innerHTML);
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+}
+
+
+function markerBounceToggle(marker){
+  if (marker.getAnimation()) {
+    marker.setAnimation(null);
+  } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function sortList(list){
   //add first result into sorted array to compare against
-  let sortedArray = [results[0]];
+  let sortedList = [list[0]];
   //compare each results rating against sortedArray rating
-  for(let i = 1; i < results.length;i++){
-    for(let j = 0; j < sortedArray.length;j++){
+  for(let i = 1; i < list.length;i++){
+    for(let j = 0; j < sortedList.length;j++){
       //if rating is more than or equal to rating, splice into array at that position
-      if (results[i].rating >= sortedArray[j].rating){
-        sortedArray.splice(j,0,results[i]);
+      if (list[i].rating >= sortedList[j].rating){
+        sortedList.splice(j,0,list[i]);
         break;
         //else, push to end if not more than or equal to any rating tested
-      } else if (j === sortedArray.length - 1){
-          sortedArray.push(results[i]);
+      } else if (j === sortedList.length - 1){
+          sortedList.push(list[i]);
           break;
         }
     }
   }
-  return(sortedArray);
+  return(sortedList);
 }
 
-function addUserMarker(map,location){
-    let userMarker = new google.maps.Marker({
-      map: map,
-      icon: 'img/user_map_pin.png',
-      title: 'Your Location',
-      position: location,
-      animation: google.maps.Animation.BOUNCE
-    });
-    return (userMarker);
-}
 
-function addPlaceMarkers(results,map){
-  let markers = [];
-  for (i = 0; i < results.length; i++){
-    let marker = new google.maps.Marker({
-      map: map,
-      icon: 'img/bfit_map_pin.png',
-      title: results[i].name,
-      position: results[i].geometry.location,
-      id: results[i].place_id
-    });
-    markers.push(marker);
-  }
-  //add infowindows on click events for each marker
-  markers.forEach(function(marker){
-    let placeInfoWindow = new google.maps.InfoWindow();
-    marker.addListener('click', function() {
-      if (placeInfoWindow.marker != this) {
-        openInfoWindow(this, placeInfoWindow, map);
-      }
-    });
+
+/*
+
+//bounce marker of selected restaurant
+  this.bounceRestaurantMarker = function(restaurant) {
+    //prevent selection of same resturant again
+    if (restaurant != selectedRestaurant){
+      selectedRestaurant = restaurant;
+      find selected restaurant marker and toggle bounce
+      markers.find(function(marker){
+        if(marker.title === restaurant.name){
+          markerBounceToggle(marker);
+          setTimeout(markerBounceToggle(marker),2000);
+        }
+      });
+    }
+  };
+
+function getPlaceID(map,location,query){
+  return new Promise(function(resolve,reject){
+    let request = {
+      locationBias: location,
+      query: query,
+      fields: ['place_id']
+    };
+    placesServiceCall = new google.maps.places.PlacesService(map);
+    placesServiceCall.findPlaceFromQuery(request,placesSearchResults);
+    function placesSearchResults(results,status){
+      if(status==='OK'){
+        resolve(results);
+      } else alert('Google Place Query has failed: '+status);
+    }
   });
-  return (markers);
 }
 
+function addPlaceMarkers(places,map){
+  let markers = [];
+  places.forEach(function(place){
+    let lat = place.lat;
+    let lng = place.lng;
+    let location = {lat,lng};
+    getPlaceID(map,location,place.name).then(function(response){
+        let id = response[0].place_id;
+        let marker = new google.maps.Marker({
+          map: map,
+          icon: 'img/bfit_map_pin.png',
+          title: place.name,
+          position: location,
+          id: id
+        });
+        markers.push(marker);
+        let placeInfoWindow = new google.maps.InfoWindow();
+        marker.addListener('click', function() {
+          if (placeInfoWindow.marker != this) {
+            openInfoWindow(this, placeInfoWindow, map);
+          }
+        });
+    }).catch(function(error){
+      alert('Google Place Query has failed with error: '+error);
+    });
+    return (markers);
+  });
+}
 
 function openInfoWindow(marker, infowindow, map) {
   let service = new google.maps.places.PlacesService(map);
@@ -173,7 +277,8 @@ function openInfoWindow(marker, infowindow, map) {
   }, function(place, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       infowindow.marker = marker;
-      let innerHTML = '<div>';
+      let innerHTML = '<div id=' + 'infowindow_container'+ '>' +
+                          '<div id='+'infowindow'+'>';
       if (place.name) {
         innerHTML += '<strong>' + place.name + '</strong>';
       }
@@ -187,14 +292,16 @@ function openInfoWindow(marker, infowindow, map) {
         innerHTML += '<br>' + '<strong>Average Rating: </strong>' + place.rating;
       }
       if (place.reviews) {
+        innerHTML += '<br>' + '<strong>Customer Reviews: </strong>';
         place.reviews.forEach(function(review){
-          innerHTML += '<br>' + '<strong>Customer Reviews: </strong>' + '<br>' + '<strong>' + review.author_name + '. Rating: ' + review.rating + '</strong>' + ' ' + review.text;
+          innerHTML += '<br>' + '<strong>' + review.author_name + '. Rating: ' + review.rating + '</strong>' + ' ' + review.text;
         })
       }
       if (place.photos) {
         innerHTML += '<br><br><img src="' + place.photos[0].getUrl({maxHeight: 100, maxWidth: 200}) + '">';
       }
-      innerHTML += '</div>';
+      innerHTML += '</div>' + '</div>';
+      //
       infowindow.setContent(innerHTML);
       infowindow.open(map, marker);
       // Make sure the marker property is cleared if the infowindow is closed.
@@ -210,37 +317,5 @@ function clearMarkers(markers){
     markers[i].setMap(null);
   }
 }
-
-function displayRoute(destination,map,location) {
-  return new Promise(function(resolve,reject){
-    let directionsServiceCall = new google.maps.DirectionsService();
-    directionsServiceCall.route({
-      origin: location,
-      destination: destination,
-      travelMode: 'WALKING',
-    }, function(response, status) {
-        if (status === google.maps.DirectionsStatus.OK) {
-          let directionsDisplay = new google.maps.DirectionsRenderer({
-              map: map,
-              directions: response,
-              draggable: true,
-              polylineOptions: {strokeColor: 'red'},
-              preserveViewport: false,
-              markerOptions: {visible:false}
-          });
-          resolve(directionsDisplay);
-        } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-      });
-  })
-}
-
-function markerBounceToggle(marker){
-  if (marker.getAnimation()) {
-    marker.setAnimation(null);
-  } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
+*/
 
---
+++ js/place_data.js
@@ -0,0 +1,34 @@
+function places(){
+  return ([
+    {
+      name: 'Adams',
+      lat: 52.4797974141386,
+      lng: -1.9014823436737063,
+      uri: 'https://api.myjson.com/bins/6v03m'
+    },
+    {
+      name: 'Damascena',
+      lat: 52.48084275912962,
+      lng: -1.8995133013523624,
+      uri: 'https://api.myjson.com/bins/1dajm2'
+    },
+    {
+      name: 'Digbeth Dining Club',
+      lat: 52.476734781332226,
+      lng: -1.883270178316746,
+      uri: 'https://api.myjson.com/bins/yckbu'
+    },
+    {
+      name: 'Fiesta del Asado',
+      lat: 52.47203668811137,
+      lng: -1.9390128832393896,
+      uri: 'https://api.myjson.com/bins/o3wq2'
+    },
+    {
+      name: 'The Highfield',
+      lat: 52.46851732475738,
+      lng: -1.922606699391428,
+      uri: 'https://api.myjson.com/bins/1e8u6i'
+    }
+  ]);
+}
